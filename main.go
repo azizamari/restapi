8dab3b8494af2a6d75bb33d0d0e6e197da80f33e
azizamari <azizamari2003@gmail.com>
2020-10-22 12:33:07 +0100

    Improved API

--- main.go
+++ main.go
@@ -3,42 +3,43 @@ package main
 import (
 	"encoding/json"
 	"log"
-	"net/http"
 	"math/rand"
-	// "strconv"
+	"net/http"
+	"strconv"
+
 	"github.com/gorilla/mux"
 )
 
-// Book Struct (Model)
+// Book struct (Model)
 type Book struct {
-	ID string `json:"id"`
-	Isbn string `json:"isbn"`
-	Title string `json:"title"`
+	ID     string  `json:"id"`
+	Isbn   string  `json:"isbn"`
+	Title  string  `json:"title"`
 	Author *Author `json:"author"`
 }
 
-// Auther Struct
+// Author struct
 type Author struct {
-	FirstName string `json:"firstname"`
-	LastName string `json:"lastname"`
+	Firstname string `json:"firstname"`
+	Lastname  string `json:"lastname"`
 }
 
 // Init books var as a slice Book struct
 var books []Book
 
-// Get all Books
-func getBooks(w http.ResponseWriter, r *http.Request){
-	w.Header().Set("Content-Type", "apllication/json")	
+// Get all books
+func getBooks(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(books)
 }
 
-// Get a specific Book
-func getBook(w http.ResponseWriter, r *http.Request){
-	w.Header().Set("Content-Type", "apllication/json")	
-	params:=mux.Vars(r) // Get params
-	// Loop through books and find with id
-	for _, item := range books{
-		if item.ID == params["id"]{
+// Get single book
+func getBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r) // Gets params
+	// Loop through books and find one with the id from the params
+	for _, item := range books {
+		if item.ID == params["id"] {
 			json.NewEncoder(w).Encode(item)
 			return
 		}
@@ -46,37 +47,69 @@ func getBook(w http.ResponseWriter, r *http.Request){
 	json.NewEncoder(w).Encode(&Book{})
 }
 
-// Create a new Book
-func createBook(w http.ResponseWriter, r *http.Request){
-	w.Header().Set("Content-Type", "apllication/json")	
+// Add new book
+func createBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
 	var book Book
-	_=json.NewDecoder(r.Body).Decode(&book)
-	// book.ID=
+	_ = json.NewDecoder(r.Body).Decode(&book)
+	book.ID = strconv.Itoa(rand.Intn(100000000)) // Mock ID - not safe
+	books = append(books, book)
+	json.NewEncoder(w).Encode(book)
 }
 
-// Update Books
-func updateBook(w http.ResponseWriter, r *http.Request){
-
+// Update book
+func updateBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range books {
+		if item.ID == params["id"] {
+			books = append(books[:index], books[index+1:]...)
+			var book Book
+			_ = json.NewDecoder(r.Body).Decode(&book)
+			book.ID = params["id"]
+			books = append(books, book)
+			json.NewEncoder(w).Encode(book)
+			return
+		}
+	}
 }
 
-// Delete a specific Book
-func deleteBooks(w http.ResponseWriter, r *http.Request){
-
+// Delete book
+func deleteBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range books {
+		if item.ID == params["id"] {
+			books = append(books[:index], books[index+1:]...)
+			break
+		}
+	}
+	json.NewEncoder(w).Encode(books)
 }
 
-func main(){
-	// Init Router
-	r:=mux.NewRouter()
+// Main function
+func main() {
+	// Init router
+	r := mux.NewRouter()
 
-	//Mock data @todo - implement DB
-	books = append(books, Book{ID: "1",Isbn: "4485",Title: "Harry Potter", Author: &Author{FirstName: "JK", LastName: "Rowling"}})
-	books = append(books, Book{ID: "2",Isbn: "49851",Title: "Rich Dad Poor Dad", Author: &Author{FirstName: "Robert", LastName: "Kyosaki"}})
+	// Hardcoded data - @todo: add database
+	books = append(books, Book{ID: "1", Isbn: "438227", Title: "Book One", Author: &Author{Firstname: "John", Lastname: "Doe"}})
+	books = append(books, Book{ID: "2", Isbn: "454555", Title: "Book Two", Author: &Author{Firstname: "Steve", Lastname: "Smith"}})
 
-	//Route Handlers / Endpoints
-	r.HandleFunc("/api/books", getBooks).Methods("GET")
-	r.HandleFunc("/api/books/{id}", getBook).Methods("GET")
-	r.HandleFunc("/api/books", createBook).Methods("POST")
-	r.HandleFunc("/api/books/{id}", updateBook).Methods("PUT")
-	r.HandleFunc("/api/books/{id}", deleteBooks).Methods("DELETE")
-	log.Fatal(http.ListenAndServe(":8000",r))
+	// Route handles & endpoints
+	r.HandleFunc("/books", getBooks).Methods("GET")
+	r.HandleFunc("/books/{id}", getBook).Methods("GET")
+	r.HandleFunc("/books", createBook).Methods("POST")
+	r.HandleFunc("/books/{id}", updateBook).Methods("PUT")
+	r.HandleFunc("/books/{id}", deleteBook).Methods("DELETE")
+
+	// Start server
+	log.Fatal(http.ListenAndServe(":8000", r))
 }
+
+// Request sample
+// {
+// 	"isbn":"4545454",
+// 	"title":"Book Three",
+// 	"author":{"firstname":"Harry","lastname":"White"}
+// }
